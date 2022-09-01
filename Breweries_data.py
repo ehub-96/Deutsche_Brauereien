90cfed49720aa7ce784f39e479aebcd0fc54c45a
Erik Huß <107818060+ehub-96@users.noreply.github.com>
2022-09-01 16:39:54 -0600

    Add files via upload

---
+++ Breweries_data.py
@@ -0,0 +1,58 @@
+Breweries_List = [{"ID": "001", "Brewry": "Weihenstephan", "State:": "Bavaria", "District/City": "Freising",
+"Year Opened": "1040", "Produced Beer #1": "Weihenstephaner Helles", "Produced Beer #2": "Hefeweißbier", 
+"Produced Beer #3": "Winterfestbier", "Produced Beer #4": "Naturradler", "Produced Beer #5": "Kellerbier",
+"Top Beer":"Weihenstephaner Kristallweißbier"},
+
+{"ID": "002", "Brewry": "Weltenburg Abbey", "State:": "Bavaria", "District/City": "Kelheim",
+"Year Opened": "1050", "Produced Beer #1": "Märzen", "Produced Beer #2": "Zwickelbier", 
+"Produced Beer #3": "Kloster", "Produced Beer #4": "Dunkelweizen", "Produced Beer #5":"Wiesnbier",
+"Top Beer":"Weltenburger Kloster Anno 1050"},
+
+{"ID": "003", "Brewry": "Bolten-Brauerei", "State:": "North Rhine-Westphalia", "District/City": "Korschenbroich",
+"Year Opened": "1266", "Produced Beer #1": "Alt", "Produced Beer #2": "Ur-Alt", 
+"Produced Beer #3": "Ur-Weizen", "Produced Beer #4": "Landbier", "Produced Beer #5":"Malz",
+"Top Beer":"BOLTENs Helles"},
+
+{"ID": "004", "Brewry": "Gaffel Becker", "State:": "North Rhine-Westphalia", "District/City": "Cologne",
+"Year Opened": "1302", "Produced Beer #1": "Weiss", "Produced Beer #2": "Kölsch", 
+"Produced Beer #3": "Fass Brause", "Produced Beer #4": "Sonnen Hopfen", "Produced Beer #5":"Lemon",
+"Top Beer":"Gaffel Koelsch"},
+
+{"ID": "005", "Brewry": "Augustiner-Bräu", "State:": "Bavaria", "District/City": "Munich",
+"Year Opened": "1328", "Produced Beer #1": "Lagerbier Hell", "Produced Beer #2": "Edelstoff", 
+"Produced Beer #3": "Heller Bock", "Produced Beer #4": "Dunkel", "Produced Beer #5":"Doppelbock",
+"Top Beer":"Augustiner Helles"},
+
+{"ID": "006", "Brewry": "Franziskaner", "State:": "Bavaria", "District/City": "Munich",
+"Year Opened": "1363", "Produced Beer #1": "Hefe-Weissbier Naturtrüb", "Produced Beer #2": "Hefe-Weissbier Dunkel", 
+"Produced Beer #3": "Weissbier Kristallklar", "Produced Beer #4": "Helles", "Produced Beer #5":"Hefe-Weissbier Leicht",
+"Top Beer":"Franziskaner Kellerbier"},
+
+{"ID": "007", "Brewry": "Spaten", "State:": "Bavaria", "District/City": "Munich",
+"Year Opened": "1372", "Produced Beer #1": "Märzenbier", "Produced Beer #2": "Pils", 
+"Produced Beer #3": "Diät-Pils", "Produced Beer #4": "Doppelbock", "Produced Beer #5":"Oktoberfestbier",
+"Top Beer":"Spaten Münchner Hell"},
+
+{"ID": "008", "Brewry": "Hofbräu", "State:": "Bavaria", "District/City": "Munich",
+"Year Opened": "1589", "Produced Beer #1": "Münchner Weisse", "Produced Beer #2": "Schwarze Weisse", 
+"Produced Beer #3": "Sommerzwickl", "Produced Beer #4": "Winterzwickl", "Produced Beer #5":"Maibock",
+"Top Beer":"Hofbräu Oktoberfestbier"},
+
+{"ID": "010", "Brewry": "Paulaner", "State:": "Bavaria", "District/City": "Munich",
+"Year Opened": "1634", "Produced Beer #1": "Weissbier", "Produced Beer #2": "Weissbier Dunkel", 
+"Produced Beer #3": "Oktoberfest Bier", "Produced Beer #4": "Salvator", "Produced Beer #5":"Hell",
+"Top Beer":"Münchner Hell"},
+
+{"ID": "011", "Brewry": "Schlenkerla", "State:": "Bavaria", "District/City": "Bamberg",
+"Year Opened": "1678", "Produced Beer #1": "Eiche", "Produced Beer #2": "Rauchweizen", 
+"Produced Beer #3": "Urbock", "Produced Beer #4": "Kräusen", "Produced Beer #5":"Fastenbier",
+"Top Beer":"Schlenkerla Jahrgangsbiere"}
+]
+
+
+
+Beer_Types =[
+    "Altbier","Bock","Berliner Weisse","Dampfbier","Doppelbock","Dortmunder","Dunkel",
+    "Eisbock","Gose","Helles","Kellerbier","Kölsch","Kottbusser","Maibock","Malzbier","Märzen","Pilsner",
+    "Rauchbier","Roggenbier","Schwarzbier","Geräuchertesbier","Weissbier","Zoigl"
+]
